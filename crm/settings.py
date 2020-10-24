2a67dafb3162dbb70a904d55777160388f135326
Rari Raj <rariraj95@gmail.com>
2020-10-25 02:03:34 +0530

    staticfiles error2

--- crm/settings.py
+++ crm/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
 
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,9 +135,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR,'crm/staticfiles')
+
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 
 MEDIA_URL='/images/'
