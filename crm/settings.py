956c9a2965b454b3838f914d8f9b805943680daa
Rari Raj <rariraj95@gmail.com>
2020-10-25 01:23:57 +0530

    secretkey error

--- crm/settings.py
+++ crm/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '%&^($o#%w2gm=$yklyz!=i^#4_llvqre#tl&m^^b7v2$oyga4$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
