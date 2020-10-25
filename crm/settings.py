4ba1e6e6d8d90eb86d02fb382f70cdbaa36cc9c2
Rari Raj <rariraj95@gmail.com>
2020-10-25 13:30:25 +0530

    gunicorn error

--- crm/settings.py
+++ crm/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
-import django_heroku
+#import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -145,7 +145,7 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 
 
